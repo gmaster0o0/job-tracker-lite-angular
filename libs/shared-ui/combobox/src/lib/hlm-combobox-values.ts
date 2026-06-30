3a19b0e47dbe0b079d1af6873aa8bb847f3fda10
Gabor Kotel <gabor.kotel@gmail.com>
2026-06-30 08:00:42 +0200

    feat: spartan combobox installed

---
+++ libs/shared-ui/combobox/src/lib/hlm-combobox-values.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+import { BrnComboboxValues } from '@spartan-ng/brain/combobox';
+
+@Directive({
+  selector: '[hlmComboboxValues]',
+  hostDirectives: [BrnComboboxValues],
+})
+export class HlmComboboxValues {}
